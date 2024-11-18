7825ccb8401642f9b5469168ee79da384f0f4ab7
AGBLA kokou Jacques <agblajack@gmail.com>
2024-11-18 19:30:00 +0100

    Initialization of the Planning Poker project
    - Creating the project on spring initializer
    - Adding dependencies

---
+++ src/test/java/com/master1/planningpoker/PlanningpokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master1.planningpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningpokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
